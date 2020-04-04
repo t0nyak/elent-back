1beba67aaf5b163e785a0d25e9ad25ccab44b6a1
Elent <elent@vps751301.ovh.net>
2020-04-04 20:05:21 +0200

    Include new files

--- trees_ong_back/settings.py
+++ trees_ong_back/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'mekfwj@h+vb2th3!vgrx3q*z00)cxa@3&tzgzsh2sw$+c#9hiu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','elent.org','www.elent.org']
 
 
 # Application definition
@@ -132,3 +132,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/home/elent/back-admin/static/'
