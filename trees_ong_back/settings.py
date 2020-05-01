2044a0708215bb6277a5a4e5eb755f8395fb2669
Toni <antonio.cancela.diaz@gmail.com>
2020-05-01 19:11:54 +0200

    Use relative path to media

--- trees_ong_back/settings.py
+++ trees_ong_back/settings.py
@@ -140,5 +140,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_ROOT = '/home/elent/back-admin/media/images'
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/images')
