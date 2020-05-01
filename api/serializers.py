dc82fe51546c4285fceb6f2156df34cd43c83fa3
Toni <antonio.cancela.diaz@gmail.com>
2020-05-01 19:05:38 +0200

    Improve serializers of shop

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,26 @@
 from rest_framework import serializers
+from django.conf import settings
 from . import models
 
 
+PROJECT_UNITS = (
+    (0, 'hectareas'),
+    (1, 'árboles'),
+    (2, 'toneladas'),
+    (3, 'euros')
+)
+
+
 class ProjectSerializer(serializers.ModelSerializer):
+    unit = serializers.SerializerMethodField('get_unit_name')
+    image = serializers.SerializerMethodField('get_image_url')
+
+    def get_unit_name(self, obj):
+        return PROJECT_UNITS[obj.unit][1]
+
+    def get_image_url(self, obj):
+        return settings.MEDIA_ROOT + '/' + obj.image.image.url
+
     class Meta:
         model = models.Project
         #fields = ('uuid', 'title', 'unit', 'description', 'visible', 'got', 'goal', 'created_on')
