5d5722d7a3045f1331a76209c9bf1bb3d3ab38ec
Toni <antonio.cancela.diaz@gmail.com>
2020-05-15 11:24:45 +0200

    Update urls with api prefix

--- trees_ong_back/urls.py
+++ trees_ong_back/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
-    path('shop/', include('shop.urls')),
-    path('api/', include('api.urls'))
+    path('api/blog/', include('blog.urls')),
+    path('api/shop/', include('shop.urls')),
+    path('api/api/', include('api.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
